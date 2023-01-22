a1b5c925a7775a3fbe93e4738a15289db18d1647
Shadab_Faisal <shadabfaisal91@gmail.com>
2023-01-23 01:38:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends React.Component {
-constructor(props) {
-  super(props);
-
-  this.state = {
+  state = {
+    advice: "",
+    isBtnClicked: false,
   };
-}
+
+  componentDidMount() {
+    //console.log('component did mount');
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        //console.log(advice);
+        this.setState({ advice, isBtnClicked: true });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   render() {
-    return <div></div>;
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1>Advice for Today!</h1>
+          {this.state.isBtnClicked ? (
+            <h1 className="heading">{advice}</h1>
+          ) : (
+            <p>Fetching an Advice for You...</p>
+          )}
+          <button className="btn" onClick={this.fetchAdvice}>
+            <span>Give me Advice!</span>
+          </button>
+        </div>
+      </div>
+    );
   }
 }
-
 
 export default App;
